72147696741806dbb43686110af09ed755cacb3a
jafarova1 <gultajvj@code.edu.az>
2023-08-07 01:03:33 +0400

    added header footer body

---
+++ FinalProject/FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ FinalProject/FinalProject/wwwroot/js/script.js
@@ -0,0 +1,37 @@
+//  const hamburger=document.querySelector(".hamburger")
+//   const navMenu=document.querySelector(".nav-menu")
+
+//   hamburger.addEventListener("click",function(){
+//     hamburger.classList.toggle("active")
+//     navMenu.classList.toggle("active")
+//   })
+
+//   document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
+//     hamburger.classList.remove("active")
+//     navMenu.classList.remove("active")
+//   }))
+
+
+
+
+
+        // var readMoreLink = document.getElementById('read-more-link');
+        // var fullContent = document.querySelector('.full-content');
+
+        // readMoreLink.addEventListener('click', function (event) {
+        //     event.preventDefault();
+
+        //     if (fullContent.style.display === 'none') {
+        //         fullContent.style.display = 'block';
+        //         readMoreLink.textContent = 'Read Less';
+        //     } else {
+        //         fullContent.style.display = 'none';
+        //         readMoreLink.textContent = 'Read More';
+        //     }
+        // });
+
+
+
+        
+
+        
